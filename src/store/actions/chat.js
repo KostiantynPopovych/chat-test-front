a022cd65411e55f3f63778f092783263ecaca330
malachitetrip <malachitetrip@gmail.com>
2019-07-12 12:30:37 +0300

    Fixed URLs

--- src/store/actions/chat.js
+++ src/store/actions/chat.js
@@ -5,13 +5,15 @@ import {
 } from '../actionCreators/chat';
 import { WEBSOCKET_PREFIX } from '../../constants/config';
 
+const isProd = true;
+const REST_URL = isProd ? 'https://chat-test-back.herokuapp.com/chat' : 'http://localhost:5000/chat';
+const WS_URL = isProd ? 'wss://chat-test-back.herokuapp.com' : 'ws://localhost:5000';
+
 export const fetchMessages = () => async (dispatch) => {
-  const { data } = await axios.get('https://chat-test-back.herokuapp.com/chat');
+  const { data } = await axios.get(REST_URL);
   dispatch(fetchChatMessagesSuccess(data));
 }
 
 export const sendWSMessage = payload => dispatch => dispatch(send({ type: 'MESSAGE', payload }, WEBSOCKET_PREFIX));
 export const loginUser = payload => dispatch => dispatch(send({ type: 'LOGIN', payload }, WEBSOCKET_PREFIX));
-const isProd = true;
-const URL = isProd ? 'wss://chat-test-back.herokuapp.com' : 'ws://localhost:5000';
-export const connectToWS = () => (dispatch) => dispatch(connect(URL, WEBSOCKET_PREFIX));
+export const connectToWS = () => (dispatch) => dispatch(connect(WS_URL, WEBSOCKET_PREFIX));
